da34dda54886eea49784c71000cac31ac894d104
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-03-26 09:27:05 +0600

    create new folder with 2 files

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,14 +1,23 @@
-import Logo from "./Logo/Logo"
-import Logo from "./Nav/Nav"
-import classes from "./Drawer.module.css"
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import Backdrop from "../UI/Backdrop/Backdrop";
+import classes from "./Drawer.module.css";
 
-const Drawer = ({}) => {
-    return (  
-        <div className={classes.Drawer}>
-<Logo/>
-<Nav/>
-        </div>
-    );
+const Drawer = ({ open, closeDrawer }) => {
+  const drawerClasses = [
+    classes.content,
+    open ? classes.open : classes.close
+  ];
+
+  return (
+    <div className={classes.Drawer}>
+      <Backdrop show={open} click={closeDrawer} />
+      <div className={drawerClasses.join(" ")}>
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
 }
  
 export default Drawer;
