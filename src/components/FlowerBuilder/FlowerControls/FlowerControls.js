d91fc353ed946b12a8c2a5258a5899d54c8d08a6
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-03-26 14:48:47 +0600

    delete some details

--- src/components/FlowerBuilder/FlowerControls/FlowerControls.js
+++ src/components/FlowerBuilder/FlowerControls/FlowerControls.js
@@ -1,11 +1,12 @@
 import classes from "./FlowerControls.module.css"
+import FlowerControl from "./FlowerControl/FlowerControl"
 
 const FlowerControls = ({ingredients}) => {
 const results =[];
 for (const ingredient in ingredients) {
-   result.push(ingredient)
+   result.push(<FlowerControl type ={ingredient}/>)
     }
-}
+
 
     return ( 
         <div className={classes.FlowerControls}>{results} </div>
