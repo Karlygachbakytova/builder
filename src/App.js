1ab9b9d63f761084e7e461f1c1103a3dd661971a
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-03-26 09:51:26 +0600

    fixed app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Layout from"./components/Layout/Layout";
-import "./App.css"
+  
+import Layout from "./components/Layout/Layout";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-    <Layout>
-     <h1> Pizza builder by master Karlygach</h1>
-    </Layout>
+      <Layout>
+        Pizza Builder by master Elaman Imashov
+      </Layout>
     </div>
   );
 }
