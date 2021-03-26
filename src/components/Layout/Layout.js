d3519576bcc9d4498a60b5f4c7dffffd0c2cdc37
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-03-26 14:03:27 +0600

    passed drawer open

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import classes from "./Layout.module.css";
-import Toolbar from "../Toolbar/Toolbar"
-import Drawer from "../Drawer/Drawer"
+import Toolbar from "../Toolbar/Toolbar";
+import Drawer from "../Drawer/Drawer";
 
-const Layout = ({children}) => {
-    return ( 
-        <div className= {classes.Layout}>
-            <Toolbar/>
-            <Drawer/>
-            {children}
+const Layout = ({ children }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-        </div>
-     );
+  return (
+    <div className={classes.Layout}>
+      <Toolbar openDrawer={() => setDrawerOpen(true)} />
+      <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
+      {children}
+    </div>
+  );
 }
  
 export default Layout;
