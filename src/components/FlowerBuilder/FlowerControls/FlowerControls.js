ca045de8683b660b0782865d69ebdcf3329da783
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-04 14:42:56 +0600

    add functions for ingredients

--- src/components/FlowerBuilder/FlowerControls/FlowerControls.js
+++ src/components/FlowerBuilder/FlowerControls/FlowerControls.js
@@ -1,16 +1,22 @@
-import classes from "./FlowerControls.module.css"
-import FlowerControl from "./FlowerControl/FlowerControl"
+import FlowerControl from "./FlowerControl/FlowerControl";
+import classes from "./FlowerControls.module.css";
 
-const FlowerControls = ({ingredients}) => {
-const results =[];
-for (const ingredient in ingredients) {
-   result.push(<FlowerControl type ={ingredient}/>)
-    }
+const FlowerControls = ({ ingredients, addIngredient, removeIngredient }) => {
+  const results = [];
+  for (const ingredient in ingredients) {
+    results.push(<FlowerControl
+        key={ingredient}
+        add={addIngredient}
+        remove={removeIngredient}
+        type={ingredient} />)
+  }
 
-
-    return ( 
-        <div className={classes.FlowerControls}>{results} </div>
-     );
+  return (
+    <div className={classes.FlowerControls}>
+      <strong>Ingredients</strong>
+      {results}
+    </div>
+  );
 }
- 
+
 export default FlowerControls;
