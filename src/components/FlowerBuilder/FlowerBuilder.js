e89586e82484ae103974e63954d0d2edf93b8702
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-06 06:29:09 +0600

    add useEffect, fix list of ingredients

--- src/components/FlowerBuilder/FlowerBuilder.js
+++ src/components/FlowerBuilder/FlowerBuilder.js
@@ -2,47 +2,59 @@ import FlowerPreview from "./FlowerPreview/FlowerPreview";
 import FlowerControls from "./FlowerControls/FlowerControls";
 
 import classes from "./FlowerBuilder.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const FlowerBuilder = () => {
-  const [ingredients, setIngredients] = useState({
-    rose: 1,
-    lily: 1,
-   orchid: 1,
-   chamomile: 1,
-   chrysanthemum: 1,
-   
-  });
+    const prices = {
+        rose: 75,
+        lily: 50,
+        orchid: 40,
+        chamomile: 15,
+        chrysanthemum: 35,
+      };
+      const [ingredients, setIngredients] = useState({
+        rose: 3,
+        lily: 3,
+        orchid: 3,
+        chamomile: 3,
+        chrysanthemum: 3,
+      });
+      
 
-useEffect (() => {
+useEffect (function()  {
     axios.get('https://builder-fd7e5-default-rtdb.firebaseio.com/ingredients.json')
-    .then ((response) => {
-        console.log(response);
-    })
-})
+    .then (response => {
+        setIngredients({ ...response.data});
+      })
+  }, []);
+
+  const [price, setPrice] = useState(150);
 
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
     newIngredients[type]++;
+    setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
   function removeIngredient(type) {
-    const newIngredients = { ...ingredients };
-    newIngredients[type]--;
-    setIngredients(newIngredients);
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setIngredients(newIngredients);
+    }
   }
 
   return (
     <div className={classes.FlowerBuilder}>
-      <FlowerPreview ingredients={ingredients} />
-      <FlowerControls
-        ingredients={ingredients}
-        addIngredient={addIngredient}
-        removeIngredient={removeIngredient}
-        />
-    </div>
+    <FlowerPreview price={price} ingredients={ingredients} />
+    <FlowerControls
+      ingredients={ingredients}
+      addIngredient={addIngredient}
+      removeIngredient={removeIngredient}
+      />
+  </div>
   );
 }
 
