8c3b9d384771c9a2ffc8caa119cda36e63ef9463
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-07 14:47:28 +0600

    add button

--- src/components/FlowerBuilder/FlowerControls/FlowerControls.js
+++ src/components/FlowerBuilder/FlowerControls/FlowerControls.js
@@ -3,7 +3,11 @@ import classes from "./FlowerControls.module.css";
 
 const FlowerControls = ({ ingredients, addIngredient, removeIngredient }) => {
   const results = [];
+  let total = 0;
   for (const ingredient in ingredients) {
+    // Add ingredient number to totals number
+    total += ingredients[ingredient];
+    // Render pizza control for this ingredient
     results.push(<FlowerControl
         key={ingredient}
         add={addIngredient}
@@ -13,10 +17,11 @@ const FlowerControls = ({ ingredients, addIngredient, removeIngredient }) => {
 
   return (
     <div className={classes.FlowerControls}>
-      <strong>Ingredients</strong>
-      {results}
-    </div>
-  );
+    <strong>Ingredients</strong>
+    {results}
+    <button disabled={!total}>Order</button>
+  </div>
+);
 }
 
 export default FlowerControls;
