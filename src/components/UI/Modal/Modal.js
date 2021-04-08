17d9a009137e086f519820db9e7fd772225c7b93
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-08 23:05:01 +0600

    create modal.js, add style

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,24 @@
+import classes from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
+import Button from "../Button/Button";
+
+const Modal = ({ show, children, cancel }) => {
+  const style = {
+    transform: show ? "translateY(0)" : "translateY(-100vh)",
+  };
+
+  return (
+    <div className={classes.Modal}>
+      <Backdrop show={show} click={cancel} />
+      <div className={classes.content} style={style}>
+        {children}
+        <div>
+          <Button onClick={cancel} green>Checkout</Button>
+          <Button onClick={cancel}>Cancel</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
