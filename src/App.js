51b8fd0bb0c9d5f103cec949d49cef35fa278f21
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-16 15:12:35 +0600

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import Layout from "./components/Layout/Layout";
 import "./App.css";
 import FlowerBuilder from "./components/FlowerBuilder/FlowerBuilder"
+import { Redirect, Route, Switch } from "react-router";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <FlowerBuilder />
+      <Switch>
+          <Route path="/" component={FlowerBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
       </Layout>
 
     </div>
