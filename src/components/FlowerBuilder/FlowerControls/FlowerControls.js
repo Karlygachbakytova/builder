5ac603621ebc8458807c5028e0b28297f1da6546
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-22 17:55:58 +0600

    rewrite controls

--- src/components/FlowerBuilder/FlowerControls/FlowerControls.js
+++ src/components/FlowerBuilder/FlowerControls/FlowerControls.js
@@ -1,17 +1,17 @@
-import Button from "../../UI/Button/Button";
-import FlowerControl from "./FlowerControl/FlowerControl";
+import Button from "../../UI/Button/Button"
+import FlowerControl from "./FlowerControl/FlowerControl"
 import classes from "./FlowerControls.module.css"
 
-const FlowerControls  = ({
-  ingredients,
-  addIngredient,
-  removeIngredient,
-  startOrdering
+const FlowerControls = ({
+    ingredients,
+    addIngredient,
+    removeIngredient,
+    startOrdering
 }) => {
-  const results = [];
-  let total = 0;
-  for (const ingredient in ingredients) {
-    // Add ingredient number to totals number
+    const results = [];
+    let total = 0;
+    for (const ingredient in ingredients) {
+         // Add ingredient number to totals number
     total += ingredients[ingredient];
     // Render pizza control for this ingredient
     results.push(<FlowerControl
@@ -19,16 +19,15 @@ const FlowerControls  = ({
         add={addIngredient}
         remove={removeIngredient}
         count={ingredients[ingredient]}
-        type={ingredient} />)
-  }
+        type={ingredient} />)   
+    }
+    return (     <div className={classes.FlowerControls}>
+        <strong>Ingredients</strong>
+        {results}
+        <Button disabled={!total} onClick={startOrdering}>Order</Button>
+        </div>
+    );
+    } 
 
-  return (
-    <div className={classes.FlowerControls}>
-    <strong>Ingredients</strong>
-    {results}
-    <Button disabled={!total} onClick={startOrdering}>Order</Button>
-    </div>
-);
-}
-
+ 
 export default FlowerControls;
