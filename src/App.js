438450cd8b71aaa6f1473e2f9c38710eb4d8a0bf
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-05-19 15:38:48 +0600

    add auth compponent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import FlowerBuilder from "./components/FlowerBuilder/FlowerBuilder"
 import { Redirect, Route, Switch } from "react-router";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
+import Auth from "./components/Auth/Auth";
+import Logout from "./components/Logout/Logout";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" component={FlowerBuilder} exact />
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
           <Redirect to="/" />
         </Switch>
       </Layout>
