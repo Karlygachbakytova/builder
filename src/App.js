4c7d8bd1687f9beb64e4da599e7060a32a3b2713
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-05-21 13:14:54 +0600

    change names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Layout from "./components/Layout/Layout";
 import "./App.css";
-import FlowerBuilder from "./components/FlowerBuilder/BouquetBuilder"
+import FlowerBuilder from "./components/FlowerBuilder/FlowerBuilder"
 import { Redirect, Route, Switch } from "react-router";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
--- src/components/FlowerBuilder/FlowerBuilder.js
+++ src/components/FlowerBuilder/FlowerBuilder.js
@@ -1,7 +1,7 @@
 import FlowerPreview from "./FlowerPreview/FlowerPreview";
 import FlowerControls from "./FlowerControls/FlowerControls";
 
-import classes from "./BouquetBuilder.module.css";
+import classes from "./FlowerBuilder.module.css";
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import Modal from "../UI/Modal/Modal";
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { load } from "../../store/actions/builder";
 import withAxios from "../withAxios";
 
-const BouquetBuilder = ({ history }) => {
+const FlowerBuilder = ({ history }) => {
   const dispatch = useDispatch();
   const ingredients = useSelector(state => state.builder.ingredients);
   const price = useSelector(state => state.builder.price);
@@ -59,4 +59,4 @@ const BouquetBuilder = ({ history }) => {
   );
 }
 
-export default withAxios(BouquetBuilder, axios);
+export default withAxios(FlowerBuilder, axios);
