eaab6d13618fbba0de80e9a270d295bc6aa489d5
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-05-26 15:07:01 +0600

    add some changes

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -6,9 +6,15 @@ const Order = ({ name, phone, address, ingredients, price }) => {
 
   return (
     <div className={classes.Order}>
-      <div>{name}, {phone}, {address}</div>
-      <div>{outputIngredients}</div>
-      <strong>{price}</strong>
+     <div>
+        <span className={classes.strong}>Name:</span> {name}
+        <br/>
+        <span className={classes.strong}>Phone: </span>{phone} 
+        <br/>
+        <span className={classes.strong}>Address: </span>{address} 
+      </div>
+      <div><span className={classes.strong}>Orders:</span><div> {outputIngredients}</div></div>
+      <strong><span className={classes.strong}>Price:</span></strong>{price}
     </div>
   );
 }
